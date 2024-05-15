d7b52970d0295568b85b8a72e1d6132ef2e65298
王梦奇 <mengqi.wang@tanovo.com>
2024-05-15 11:46:48 +0800

    feat: vue3基础

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+    <el-tab-pane
+      v-for="(item, index) in tabList"
+      :label="item.label"
+      :key="index"
+      :name="item.name">
+      <component :is="item.component" />
+    </el-tab-pane>
+  </el-tabs>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
+import HelloWorld from './components/HelloWorld.vue'
+import Vue3 from './components/Vue3.vue'
+import BasicGrammar from './components/BasicGrammar.vue'
+const activeName = ref('vue3')
+const tabList = [
+  { label: 'Vue3 新特性', name: 'vue3', component: Vue3 },
+  { label: '基础语法与模板', name: 'basicGrammar', component: BasicGrammar },
+  { label: 'Composition API', name: 'composition', component: HelloWorld },
+  { label: '组件化开发', name: 'template', component: HelloWorld },
+]
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
+</script>
+
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
---
+++ src/components/Vue3.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="increment">增加</button>
+  <p>count：{{ count }}</p>
+  <p>computed：{{ doubleCount }}</p>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed, onMounted } from 'vue'
+
+// 在setup中直接定义响应式变量和计算属性
+const count = ref(0)
+const doubleCount = computed(() => count.value * 2)
+
+// 直接定义函数
+const increment = () => {
+  count.value++
+}
+
+// 使用onMounted代替mounted钩子
+onMounted(() => {
+  console.log('mounted', count.value) // 0
+})
+</script>
